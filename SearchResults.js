9991d5de5845396ef03ca07df5bc659ec8b6e1b7
Al Fair <thealisonfair@gmail.com>
2017-10-12 16:33:43 -0400

    a touch of style

--- SearchResults.js
+++ SearchResults.js
@@ -10,18 +10,49 @@ import {
   Text,
 } from 'react-native';
 
-export default class SearchResults extends Component {
-  _keyExtractor = (item, index) => index;
+class ListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.onPressItem(this.props.index);
+  }
 
-  _renderItem = ({item}) => {
+  render() {
+    const item = this.props.item;
+    const price = item.price_formatted.split(' ')[0];
     return (
       <TouchableHighlight
+        onPress={this._onPress}
         underlayColor='#dddddd'>
         <View>
-          <Text>{item.title}</Text>
+          <View style={styles.rowContainer}>
+            <Image style={styles.thumb} source={{ uri: item.img_url }} />
+            <View style={styles.textContainer}>
+              <Text style={styles.price}>{price}</Text>
+              <Text style={styles.title}
+                numberOfLines={1}>{item.title}</Text>
+            </View>
+          </View>
+          <View style={styles.separator}/>
         </View>
       </TouchableHighlight>
     );
+  }
+}
+
+export default class SearchResults extends Component {
+  _keyExtractor = (item, index) => index;
+
+  _renderItem = ({item, index}) => {
+    return (
+      <ListItem
+      item={item}
+      index={index}
+      onPressItem={this._onPressItem}
+    />
+    )
+  };
+
+  _onPressItem = (index) => {
+    console.log("Pressed row: " +index);
   };
 
   render() {
@@ -34,3 +65,37 @@ export default class SearchResults extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  thumb: {
+    width: 80,
+    height: 80,
+    marginRight: 10
+  },
+
+  textContainer: {
+    flex: 1
+  },
+
+  separator: {
+    height: 1,
+    backgroundColor: '#dddddd'
+  },
+
+  price: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#48BBEC'
+  },
+
+  title: {
+    fontSize: 20,
+    color: '#656565'
+  },
+
+  rowContainer: {
+    flexDirection: 'row',
+    padding: 10
+  },
+
+});
