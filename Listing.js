4bee10553f154de55356fa66de5fba19668caa79
Al Fair <thealisonfair@gmail.com>
2017-10-12 23:18:02 -0400

    detail_page

--- Listing.js
+++ Listing.js
@@ -13,37 +13,41 @@ import {
 export default class Listing extends Component {
 
   render() {
-    var listing = this.props.listing
+    const listing = this.props.listing
+    const price = listing.price_formatted.split(' ')[0];
     console.log(listing.img_url)
     return (
       <View style={styles.container}>
-      <Image style={styles.thumb} source={{ uri: listing.img_url }} />
-        <Text style={styles.description}>{listing.price}</Text>
+      <Text style={styles.title}>{listing.title}</Text>
+      <Image style={styles.image} source={{ uri: listing.img_url }} />
+        <Text style={styles.price}>Price: {price}</Text>
+        <Text style={styles.stats}>{listing.bedroom_number} bedroom, {listing.bathroom_number} bathroom {listing.property_type}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  description: {
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565',
-    marginTop: 65,
+
+  title: {
+    fontSize: 25,
+  },
+
+  stats: {
+    marginRight: 10,
+  },
+
+  price: {
+    fontSize: 25,
+    textAlign: 'left',
   },
 
   container: {
     flex: 1,
+    paddingTop: 65,
   },
 
   image: {
-    width: 300,
-  },
-
-  thumb: {
-    width: 400,
     height: 300,
-    marginLeft: 20,
-    marginTop: 100
   },
 });
